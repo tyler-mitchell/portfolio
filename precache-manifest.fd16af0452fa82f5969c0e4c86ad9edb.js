16158101ddc08dfd958e31129b371a94ef871ab1
Tyler Mac <tdmam404@outlook.com>
2020-05-22 16:46:02 -0500

    Updates

---
+++ precache-manifest.fd16af0452fa82f5969c0e4c86ad9edb.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44326dcc429772e7921a32b0fb853c93",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "70fe9eabdea8c9bed4fa",
+    "url": "/portfolio/static/css/main.4ed9bddc.chunk.css"
+  },
+  {
+    "revision": "8840407154c71c80092e",
+    "url": "/portfolio/static/js/2.809da205.chunk.js"
+  },
+  {
+    "revision": "221bcfd3f190ca82ebb8b28a2c527847",
+    "url": "/portfolio/static/js/2.809da205.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70fe9eabdea8c9bed4fa",
+    "url": "/portfolio/static/js/main.faaa3a4f.chunk.js"
+  },
+  {
+    "revision": "9c7b2d41a54db081cddd",
+    "url": "/portfolio/static/js/runtime-main.c7b8abef.js"
+  },
+  {
+    "revision": "bd34462c89d3a928b72682c17f78423b",
+    "url": "/portfolio/static/media/Github.bd34462c.svg"
+  },
+  {
+    "revision": "8243d3df5cc72e8f70dbb4918150e8a9",
+    "url": "/portfolio/static/media/LinkedIN.8243d3df.svg"
+  },
+  {
+    "revision": "ce29d23e0b8382e3209455e8452ab48e",
+    "url": "/portfolio/static/media/Mail.ce29d23e.svg"
+  },
+  {
+    "revision": "27705fcfbc03542fb869712736665971",
+    "url": "/portfolio/static/media/Twitter.27705fcf.svg"
+  },
+  {
+    "revision": "7766264753eb06875a6df441843e4676",
+    "url": "/portfolio/static/media/amita.77662647.png"
+  },
+  {
+    "revision": "1811afdb148ea16af6969a201196ddf4",
+    "url": "/portfolio/static/media/c.1811afdb.svg"
+  },
+  {
+    "revision": "c3bafdb5f9122db0a3eacfb153479f69",
+    "url": "/portfolio/static/media/c4d.c3bafdb5.svg"
+  },
+  {
+    "revision": "aac116a20cc1459d0788cfc067a7751f",
+    "url": "/portfolio/static/media/docker.aac116a2.svg"
+  },
+  {
+    "revision": "1c4f37801e58491b4778dcece69baadd",
+    "url": "/portfolio/static/media/figma.1c4f3780.svg"
+  },
+  {
+    "revision": "3b5d235109098e19f106a9d7c6e94c8f",
+    "url": "/portfolio/static/media/firebase.3b5d2351.svg"
+  },
+  {
+    "revision": "fae6149e8456f22ba7d6a424b3df44a0",
+    "url": "/portfolio/static/media/framer.fae6149e.svg"
+  },
+  {
+    "revision": "d95317379726ba9d62bd825e95110d17",
+    "url": "/portfolio/static/media/git.d9531737.svg"
+  },
+  {
+    "revision": "176731bfac21d9941976d696c11e966a",
+    "url": "/portfolio/static/media/ideasa.176731bf.png"
+  },
+  {
+    "revision": "cce27e614f7e248cfc9827467f46c01f",
+    "url": "/portfolio/static/media/java.cce27e61.svg"
+  },
+  {
+    "revision": "74e01e0e6e00b30b111430a230b982e9",
+    "url": "/portfolio/static/media/javascript.74e01e0e.svg"
+  },
+  {
+    "revision": "031061420c86dd69828a815abc0400bf",
+    "url": "/portfolio/static/media/keyhints.03106142.png"
+  },
+  {
+    "revision": "4412af2fd9d31d2af5ee57abaa046958",
+    "url": "/portfolio/static/media/python.4412af2f.svg"
+  },
+  {
+    "revision": "e34a2f7e61334209079441177d05b1c4",
+    "url": "/portfolio/static/media/react.e34a2f7e.svg"
+  },
+  {
+    "revision": "2b8d0c9093ea9db35222ad989ccceb15",
+    "url": "/portfolio/static/media/refinder.2b8d0c90.png"
+  },
+  {
+    "revision": "95b99ca1c3f79ad3022840ab769067cd",
+    "url": "/portfolio/static/media/shadow.95b99ca1.jpg"
+  },
+  {
+    "revision": "6110e825a83d1655d7f2777a1e8bc3fd",
+    "url": "/portfolio/static/media/tyler.6110e825.svg"
+  },
+  {
+    "revision": "692b66b54608a54e359ee8f836753f58",
+    "url": "/portfolio/static/media/typescript.692b66b5.svg"
+  }
+]);
